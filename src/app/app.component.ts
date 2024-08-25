9251029734101f23b499cfab29e540def41effd1
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-25 16:20:15 +0300

    feat: use RxJS operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { filter, interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +10,16 @@ export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
-    const subscription = interval(1000).subscribe({
-      next: (value) => console.log(`Current second: ${value}`),
-      complete: () => console.log('Interval complete'),
-      error: (error) => console.error('An error occurred:', error),
-    });
+    const subscription = interval(1000)
+      .pipe(
+        map((value) => value * 3),
+        filter((value) => value % 2 === 0)
+      )
+      .subscribe({
+        next: (value) => console.log(`Current second: ${value}`),
+        complete: () => console.log('Interval complete'),
+        error: (error) => console.error('An error occurred:', error),
+      });
 
     // Unsubscribe when the component is destroyed
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
