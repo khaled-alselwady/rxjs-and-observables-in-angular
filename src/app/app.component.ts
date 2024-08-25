de642e94a8ac2b2594dae45e4e1f52b962a73cd5
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-25 18:48:28 +0300

    feat: use signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  effect,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { filter, interval, map } from 'rxjs';
 
 @Component({
@@ -8,20 +15,30 @@ import { filter, interval, map } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
+  clickCount = signal(0);
+
+  constructor() {
+    effect(() => {
+      console.log(`Clicked: ${this.clickCount()} times.`);
+    });
+  }
 
   ngOnInit(): void {
-    const subscription = interval(1000)
-      .pipe(
-        map((value) => value * 3),
-        filter((value) => value % 2 === 0)
-      )
-      .subscribe({
-        next: (value) => console.log(`Current second: ${value}`),
-        complete: () => console.log('Interval complete'),
-        error: (error) => console.error('An error occurred:', error),
-      });
+    // const subscription = interval(1000)
+    //   .pipe(
+    //     map((value) => value * 3),
+    //     filter((value) => value % 2 === 0)
+    //   )
+    //   .subscribe({
+    //     next: (value) => console.log(`Current second: ${value}`),
+    //     complete: () => console.log('Interval complete'),
+    //     error: (error) => console.error('An error occurred:', error),
+    //   });
+    // // Unsubscribe when the component is destroyed
+    // this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 
-    // Unsubscribe when the component is destroyed
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  onClick() {
+    this.clickCount.update((prevCount) => prevCount + 1);
   }
 }
