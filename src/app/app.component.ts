5d991372419eb5e091398ba7a0ee53fd81cc0004
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-25 16:03:57 +0300

    feat: create and use interval observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
 
+  ngOnInit(): void {
+    const subscription = interval(1000).subscribe({
+      next: (value) => console.log(`Current second: ${value}`),
+      complete: () => console.log('Interval complete'),
+      error: (error) => console.error('An error occurred:', error),
+    });
+
+    // Unsubscribe when the component is destroyed
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
